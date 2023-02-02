02a7e58d48d3117b46eeab2a97f6a56e27f6f01f
kwonmw12 <kwonmw12@gmail.com>
2023-02-02 18:06:14 +0900

    갤러리부분 Mui를 활용하여 이미지리스트 작업중

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function Footer(props) {
-  return (
-    <div>
-      
-    </div>
-  );
+const Footer = () =>{
+  return(
+    <footer id="foot" className="py-4 border-top bg-dark text-white ">
+      <ul className="sosial d-flex justify-content-center">
+        <li className="px-5 "><a href="https://www.instagram.com/noeul__film/">Instagram</a></li>
+        <li className="px-5"><a href="">Kakao</a></li>
+      </ul>
+    </footer>
+  )
 }
+
 
 export default Footer;
--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Gallerycontent from './Gallerycontent';
+
 
 function Gallery(props) {
+  const [gallerypt, photoupdate] = useState(['웨딩', 'gallery_w']);
+  const btnupdate = (e) => {
+    e = e || window.event;
+    const newphotonm = e.target.
+
+      getAttribute('datasrc').split(',');
+    photoupdate(newphotonm);
+  }
   return (
-    <div>
-      
-    </div>
+    <section className="section text-center" id={props.contentid}>
+      <h3 data-aos="fade-right" className={props.title == 'no' ? 'd-none' : 'swiper_h3'}>GALLERY</h3>
+      <p data-aos="fade-left" className={props.title == 'no' ? 'd-none' : 'swiper_p'}>노을필름의 다양한 사진들을 소개합니다.</p>
+      <div className="d-flex justify-content-center">
+        <button onClick={btnupdate} datasrc={['웨딩', 'gallery_w']} className="border-none !important">웨딩스냅</button>
+        <button onClick={btnupdate} datasrc={['커플', 'gallery_c']}>커플스냅</button>
+        <button onClick={btnupdate} datasrc={['우정', 'gallery_f']}>우정스냅</button>
+        <button onClick={btnupdate} datasrc={['개인', 'gallery_p']}>개인 프로필</button>
+      </div>
+
+      <Gallerycontent dbobjkey={gallerypt[0]} galleryId={gallerypt[1]}>
+      </Gallerycontent>
+
+
+
+    </section>
   );
 }
 
